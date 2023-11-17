0731a2e3fd11c7ea588590f6446b54420ab5e9f0
Fischer Moseley <42497969+fischermoseley@users.noreply.github.com>
2023-11-17 15:09:05 -0800

    add random base address test to memory core

--- test/test_mem_core.py
+++ test/test_mem_core.py
@@ -6,7 +6,7 @@ config = {"type": "memory", "width": 16, "depth": 1024}
 mem_core = ReadOnlyMemoryCore(config, base_addr=0, interface=None)
 
 
-def fill_mem_from_user():
+def fill_mem_from_user_port(mem_core):
     for i in range(1024):
         yield mem_core.user_addr.eq(i)
         yield mem_core.user_data.eq(i)
@@ -16,7 +16,7 @@ def fill_mem_from_user():
 
 def test_bus_read_sequential():
     def testbench():
-        yield from fill_mem_from_user()
+        yield from fill_mem_from_user_port(mem_core)
         for i in range(1024):
             yield from verify_register(mem_core, i, i)
 
@@ -25,8 +25,21 @@ def test_bus_read_sequential():
 
 def test_bus_read_random():
     def testbench():
-        yield from fill_mem_from_user()
+        yield from fill_mem_from_user_port(mem_core)
         for i in sample(range(1024), k=1024):
             yield from verify_register(mem_core, i, i)
 
     simulate(mem_core, testbench, vcd_path="mem_core.vcd")
+
+
+def test_random_base_addr():
+    base_addr = randint(0, 2**16 - 1 - 1024)
+    random_mem_core = ReadOnlyMemoryCore(config, base_addr, interface=None)
+
+    def testbench():
+        yield from fill_mem_from_user_port(random_mem_core)
+
+        for i in range(1024):
+            yield from verify_register(random_mem_core, i + base_addr, i)
+
+    simulate(random_mem_core, testbench, vcd_path="mem_core.vcd")
